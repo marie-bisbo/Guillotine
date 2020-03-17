dd86d8f73de88ffe4554acf473ea3983aef6c09f
marie-brainlabs <marie@brainlabsdigital.com>
2020-03-17 15:20:59 +0000

    Add a navbar component and change App.vue to a blank page with the navbar

---
+++ guillotine/frontend/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <v-app-bar dark>
+      <v-toolbar-title>Guillotine</v-toolbar-title>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
