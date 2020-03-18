ce0c254c54ac350dc7633900323a218c58bf2c1a
marie-brainlabs <marie@brainlabsdigital.com>
2020-03-18 08:40:19 +0000

    Add a logout button with a tooltip

--- guillotine/frontend/src/components/Navbar.vue
+++ guillotine/frontend/src/components/Navbar.vue
@@ -2,6 +2,15 @@
   <nav>
     <v-app-bar dark>
       <v-toolbar-title>Guillotine</v-toolbar-title>
+      <v-spacer />
+      <v-tooltip bottom color="tooltip">
+        <template v-slot:activator="{ on }">
+          <v-btn href="accounts/login/" icon v-on="on">
+            <v-icon>mdi-logout</v-icon>
+          </v-btn>
+        </template>
+        Log Out
+      </v-tooltip>
     </v-app-bar>
   </nav>
 </template>
