c0ae261141e60fc2122415b2f8da0cbb6fad9df2
marie-brainlabs <marie@brainlabsdigital.com>
2020-03-19 14:11:44 +0000

    Add a card with an upload button

--- guillotine/frontend/src/App.vue
+++ guillotine/frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <v-app>
     <Navbar />
+    <UserInputCard />
   </v-app>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import UserInputCard from "./components/UserInputCard.vue";
 
 export default {
   name: "app",
   components: {
-    Navbar
+    Navbar,
+    UserInputCard
   }
 };
 </script>
---
+++ guillotine/frontend/src/components/UserInputCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card class="mx-auto my-10" height="600" width="600">
+    <v-tooltip bottom color="tooltip">
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on">
+          <v-icon>mdi-upload</v-icon>
+        </v-btn>
+      </template>
+      Upload images
+    </v-tooltip>
+  </v-card>
+</template>
