d07bcea78c1f7c4ec937a8f1c9c6ade7ec81109b
marie-brainlabs <marie@brainlabsdigital.com>
2020-04-10 09:46:44 +0100

    Make migrations

--- guillotine/accounts/migrations/0001_initial.py
+++ guillotine/accounts/migrations/0001_initial.py
@@ -9,18 +9,31 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-    ]
+    dependencies = [migrations.swappable_dependency(settings.AUTH_USER_MODEL)]
 
     operations = [
         migrations.CreateModel(
-            name='UserProfile',
+            name="UserProfile",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('refresh_token', models.CharField(default='', max_length=255)),
-                ('avatar', models.CharField(max_length=500)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='profile', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("refresh_token", models.CharField(default="", max_length=255)),
+                ("avatar", models.CharField(max_length=500)),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="profile",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
-        ),
+        )
     ]
